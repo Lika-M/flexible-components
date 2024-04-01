bdef5ec5509ac44d19b6d02d60a8d2ea1fa24d87
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-04-01 14:57:31 +0300

    Built a wrapper Input component using ComponentPropsWithoutRef.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Input from './components/Input.tsx'
 import './App.css'
 
 function App() {
 
   return (
     <main>
-      Welcome!
+      <>
+      {/* different types with different props */}
+        <Input label='Your name' id='name' type='text' />
+        <Input label='Yor age' id='age' type='number' />
+        <Input label='Button' id='button' type='submit' disabled />
+      </>
     </main>
   )
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type InputProps = {
+    id: string;
+    label: string;
+} & ComponentPropsWithoutRef<'input'>
+
+export default function Input({ id, label, ...rest }: InputProps) {
+    return (
+        <p>
+            <label htmlFor={id}>{label}</label>
+            <input id={id} {...rest} />
+        </p>
+    );
+}
