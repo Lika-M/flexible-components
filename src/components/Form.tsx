6f47448d6edeb8cb166278dcb7d2e6f58c0dd712
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-04-02 11:57:18 +0300

    Added useRef to reset the form.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { type FormEvent, type ComponentPropsWithoutRef, type FC } from "react";
+import { type FormEvent, type ComponentPropsWithoutRef, type FC, useRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void;
@@ -6,16 +6,20 @@ type FormProps = ComponentPropsWithoutRef<'form'> & {
 
 const Form: FC<FormProps> = ({ children, onSave, ...restProps }) => {
 
+    const form = useRef<HTMLFormElement>(null);
+
     function handleSubmit(ev: FormEvent<HTMLFormElement>) {
         ev.preventDefault();
 
         const formData = new FormData(ev.currentTarget);
         const data = Object.fromEntries(formData);
         onSave(data);
+
+        form.current?.reset();
     }
 
     return (
-        <form onSubmit={handleSubmit}{...restProps}>
+        <form onSubmit={handleSubmit}{...restProps} ref={form}>
             {children}
         </form>
     );
